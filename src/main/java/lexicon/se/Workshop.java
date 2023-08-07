6a27c84cf9c9ca2797c78b73a046c5d265d4079f
AnnA <anahita2012rad@gmail.com>
2023-08-07 23:46:24 +0200

    ArrayNames

--- src/main/java/lexicon/se/Workshop.java
+++ src/main/java/lexicon/se/Workshop.java
@@ -1,28 +1,125 @@
 package lexicon.se;
 
+/**
+ * Hello world!
+ */
 public class Workshop {
+
+    static String[] names = new String[10];
+    static NameRepositoryByChatGPT repository = new NameRepositoryByChatGPT();
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
-        String[] myArray = new String[3];
+        // In this part we have used our implementation
+        /*fillNames("Erik Svensson", names);
+        fillNames("Mehrdad Javan", names);
+        fillNames("Anna Raad", names);
+        fillNames("Azadeh Samadi", names);*/
 
-        myArray[0] = "Erik";
-        myArray[1] = " ";
-        myArray[2] = "Svensson";
-        for (String word : myArray) {
-            System.out.print(word);
+        //add("Erik Svensson");
+        //add("Mehrdad Javan");
+        //add("Erik Svensson");
+        //add("Anna Raad");
+        //add("Azadeh Samadi");
+
+        //System.out.println(find("Anna Raad"));
+        //System.out.println(find("Erik Svensson"));
+
+        //printNames(names);
+
+        // In this part we have used ChatGPT Implementation
+        repository.add("Erik Svensson");
+        repository.add("Erik Andersson");
+        repository.add("Mehrdad Javan");
+        repository.add("Patrik Andersson");
+        String[] result1 = repository.findByFirstName("Erik");
+        printNames(result1);
+
+        String[] result2 = repository.findByLastName("Andersson");
+        printNames(result2);
+
+        String result3 = repository.find("Patrik Andersson");
+        System.out.println(result3);
+
+        String[] result4 = repository.getAllNames();
+        printNames(result4);
+
+        boolean updateResult = repository.update("Erik Svensson",
+                "Rosa Svensson");
+        System.out.println(updateResult ? "true": "false");
+        printNames(repository.getAllNames());
+
+        boolean removeResult = repository.remove("Patrik Andersson");
+        printNames(repository.getAllNames());
+
+    }
+
+    private static void printNames(String[] repoArray) {
+        for (int i = 0; i < repoArray.length; i++) {
+
+            if (repoArray[i] != null && repoArray[i] != "") {
+                System.out.println(repoArray[i]);
+            }
+
+        }
+    }
+
+    private static void fillNames(String name, String[] repository) {
+
+        if (repository == null) {
+            repository = new String[10];
+        } else {
+            for (int i = 0; i < repository.length; i++) {
+
+                if (repository[i] == null || repository[i] == "") {
+                    repository[i] = name;
+                    break;
+                }
+            }
         }
 
-        String s = find("Eric Svensson");
     }
 
     public static String find(final String fullName) {
-
-        return null;
+        for (int i = 0; i < names.length; i++) {
+            if (names[i] == fullName) {
+                return names[i] + " found successfully";
+            }
+        }
+        return fullName + " not found";
     }
 
+    private static boolean add(final String fullName) {
 
+        for (int i = 0; i < names.length; i++) {
+            if (names[i] == fullName) {
+                System.out.println(fullName + " is duplicated");
+                return false;
+            }
+        }
+
+        for (int i = 0; i < names.length; i++) {
+
+            if (names[i] == null || names[i] == "") {
+                names[i] = fullName;
+                System.out.println(fullName + " added successfully");
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
